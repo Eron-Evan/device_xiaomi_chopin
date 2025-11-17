221158e14a3e67a22db76bb000ccb70691ba1e5c
rio004 <riyon336@gmail.com>
2025-11-17 00:36:10 +0000

    chopin: Decommonise NFC permissions
    * Only ares/chopin supports it
    
    Change-Id: I8ab0de15ff0fb0c504a460b5b5fc6ad31f24863b
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,14 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
 
+# NFC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/$(PRODUCT_DEVICE)/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/$(PRODUCT_DEVICE)/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/$(PRODUCT_DEVICE)/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/$(PRODUCT_DEVICE)/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/$(PRODUCT_DEVICE)/com.android.nfc_extras.xml
+
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksResOverlayAres \
