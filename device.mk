c9d305572e3a5ddff1c420c72fa7080b6fb7c1ca
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-12-19 10:28:28 +0000

    Revert "chopin: Import display config"
    This reverts commit 8aad0544a3322bbc8325102cbcdcd50494c1344e.

--- device.mk
+++ device.mk
@@ -3,9 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Display
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
 
 # NFC
 PRODUCT_COPY_FILES += \
