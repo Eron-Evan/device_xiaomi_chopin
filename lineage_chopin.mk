e4fbea8b231d258084959154425d8946a3e2cb83
Eron Evan <eronevan007@gmail.com>
2026-03-21 08:50:58 +0000

    chopin: crDroid Bringup

--- lineage_chopin.mk
+++ lineage_chopin.mk
@@ -7,6 +7,7 @@
 $(call inherit-product, device/xiaomi/chopin/device.mk)
 
 # Inherit LineageOS product
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_chopin
@@ -21,3 +22,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint= POCO/chopin_global/chopin:12/SP1A.210812.016/V14.0.6.0.TKPMIXM:user/release-keys \
     DeviceProduct=chopin \
     SystemName=chopin
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2460
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# crDroid flags
+TARGET_ENABLE_BLUR := true
+TARGET_DISABLE_MATLOG := true
+PERF_ANIM_OVERRIDE := true
+
